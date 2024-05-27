0ec68f7faa9feaa234112ef73699ee07a949edd8
Mellonnin <falciano.marco.25@gmail.com>
2024-05-27 12:01:32 +0200

    get api funzionante

--- api/api.go
+++ api/api.go
@@ -1,96 +1,129 @@
 package main
 
 import (
-	"os"
+	"database/sql"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/go-sql-driver/mysql"
+	"log"
 	"net/http"
-    "fmt"
-    "encoding/json"
-    "github.com/go-sql-driver/mysql"
-    //"io/ioutil"
 )
 
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
+var db *sql.DB
+
+type Pagina struct {
+	Titolo string
+	Body   string
 }
 
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
-
-
-func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
-}
-
-func postAlbums(c *gin.Context) {
-	var newAlbum album
-	if err := c.BindJSON(&newAlbum); err != nil {
-	    return
+func controlla(titolo string) int {
+	var tito string
+	row := db.QueryRow("SELECT titolo FROM pagine WHERE titolo = ?", titolo)
+	errore_scan := row.Scan(&tito)
+	if errore_scan == sql.ErrNoRows {
+		return 1 // pagina non trovata
 	}
-	albums = append(albums, newAlbum)
-	c.IndentedJSON(http.StatusCreated, newAlbum)
-    save(albums[len(albums)-1])
+	return 0 // trovata una pagina
 }
 
-func getAlbumByID(c *gin.Context) {
-	id := c.Param("id")
-	for _, a := range albums {
-		if a.ID == id {
-			c.IndentedJSON(http.StatusOK, a)
-			return
-		}
+//func aggiungiPagina(c *gin.Context) error {
+//    titolo:= c.Param("titolo")
+//	errore_controlla := controlla(pag.Titolo)
+//	if errore_controlla == 1 {
+//		result, errore_insert := db.Exec("INSERT INTO pagine (titolo, body) VALUES (?, ?)", pag.Titolo, pag.Body)
+//		if errore_insert != nil {
+//			return errore_insert
+//		}
+//		fmt.Println(result)
+//	} else if errore_controlla == 0 {
+//		result, errore_update := db.Exec("UPDATE pagine SET body = ? WHERE titolo = ?", pag.Body, pag.Titolo)
+//		fmt.Println(result)
+//		if errore_update != nil {
+//			return errore_update
+//		}
+//	}
+//	return nil
+//}
+
+//func (p *Pagina) salva (titolo string){
+//	err := aggiungiPagina(Pagina{
+//		Titolo: titolo,
+//		Body:  p.Body,
+//	})
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+//	fmt.Println("query fatta")
+//}
+
+//func loadPage(titolo string) (*Pagina, error) {
+//	var pag Pagina
+//	row := db.QueryRow("SELECT body FROM pagine WHERE titolo = ?", titolo)
+//	err := row.Scan(&pag.Body)
+//	if err != nil {
+//		return nil, fmt.Errorf("pagina non trovata : %v", err)
+//	}
+//	return &Pagina{Titolo: titolo, Body: pag.Body}, nil
+//}
+
+//func newHandler(titolo string) {
+//	errore_controlla := controlla(titolo)
+//
+//		p, err := loadPage(titolo)
+//		if err != nil {
+//			p = &Pagina{Titolo: titolo}
+//		}
+//    if errore_controlla == 1 {
+//        postPagine(titolo)
+//	} else if errore_controlla == 0 {
+//        getPagine(titolo)
+//    }
+//}
+
+func getPagine(c *gin.Context) {
+	titolo := c.Param("titolo")
+	if controlla(titolo) == 0 {
+		var pag Pagina
+		var row = db.QueryRow("SELECT titolo, body FROM pagine WHERE titolo = ?", titolo)
+		row.Scan(&pag.Titolo, &pag.Body)
+		c.IndentedJSON(http.StatusOK, pag)
+	} else if controlla(titolo) == 1 {
+		c.IndentedJSON(http.StatusOK, "la pagina non esiste")
 	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
 
-func save(p album) {
-	file, err := os.Create("people.json")
+func postPagine(c *gin.Context) {
+	//completa metti il content della pagina
+	// modifica il content della pagina
+}
+
+func connessioneDB() {
+	cfg := mysql.Config{
+		User:                 "mellonnin",
+		Passwd:               "arch",
+		Net:                  "tcp",
+		Addr:                 "localhost:3306",
+		DBName:               "wiki",
+		AllowNativePasswords: true,
+	}
+
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
 	if err != nil {
-		fmt.Println("Errore durante la creazione del file:", err)
-		return 
+		log.Fatal(err)
 	}
-	defer file.Close()
 
-	encoder := json.NewEncoder(file)
-	err = encoder.Encode(albums)
-	if err != nil {
-		fmt.Println("Errore durante la scrittura JSON:", err)
-		return 
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
 	}
-	fmt.Println("Dati JSON scritti su file 'people.json'")
-    return 
+	fmt.Println("connesso!")
 }
 
-func read()  {
-	// Leggere i dati JSON da un file
-	fileData, err := ioutil.ReadFile("people.json")
-	if err != nil {
-		fmt.Println("Errore durante la lettura del file:", err)
-		return
-	}
-
-	var peopleFromFile []Person
-	err = json.Unmarshal(fileData, &peopleFromFile)
-	if err != nil {
-		fmt.Println("Errore durante il parsing JSON:", err)
-		return
-	}
-
-	fmt.Println("Dati JSON letti dal file:")
-	for _, p := range peopleFromFile {
-		fmt.Printf("Nome: %s, Età: %d, Email: %s\n", p.Name, p.Age, p.Email)
-	}
-}
 func main() {
+	connessioneDB()
 	router := gin.Default()
-	router.GET("/albums", getAlbums)
-	router.POST("/albums", postAlbums)
-    router.GET("/albums/:id", getAlbumByID)
+	router.GET("/pagine/:titolo", getPagine)
+	router.POST("/pagine", postPagine)
 	router.Run("localhost:8090")
 }
